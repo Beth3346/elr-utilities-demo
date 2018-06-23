fe5fb2a37599022ee7793f62b4c894f0a903b279
Elizabeth Rogers <beth3346@gmail.com>
2018-06-23 16:15:38 -0500

    add about view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './Header.css';
 
-class Header extends Component {
-  render() {
-    const style = {
-      backgroundColor: '#333',
-      padding: '40px 0',
-      color: '#fff'
-    };
-    const titleStyle = {
-      fontSize: '42px',
-      fontWeight: '900',
-      padding: '10px 0'
-    };
-    const introStyle = {
-      fontSize: '18px',
-      padding: '10px 0'
-    };
-    return (
-      <header className="App-header" style={style}>
-        <div className="header-inner">
-          <h1 className="App-title" style={titleStyle}>
-            {this.props.heading}
-          </h1>
-          <p className="App-intro" style={introStyle}>
-            {this.props.children}
-          </p>
-        </div>
-      </header>
-    );
-  }
-}
+const Header = props => {
+  return (
+    <header className="App-header">
+      <div className="header-inner">
+        <h1 className="App-title">{props.heading}</h1>
+        <p className="App-intro">{props.children}</p>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
