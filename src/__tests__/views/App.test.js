bef0e5862f57409aa3d02266c924c53d1f6f3288
Elizabeth Rogers <beth3346@gmail.com>
2018-06-24 15:32:25 -0500

    fix tests

--- src/__tests__/views/App.test.js
+++ src/__tests__/views/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { shallow, render } from 'enzyme';
 
-import App from '@/views/App/App';
-import Header from '@/components/Header/Header';
+import { BrowserRouter } from 'react-router-dom';
+import App from '@/App';
 import NavigationMain from '@/components/NavigationMain/NavigationMain';
-import ContentMain from '@/components/ContentMain/ContentMain';
 
 let app;
 
@@ -14,20 +13,16 @@ beforeEach(() => {
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  render(<App />, div);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    div
+  );
 });
 
 describe('layout', () => {
   it('has navigation', () => {
     expect(app.find(NavigationMain).length).toBe(1);
-    expect(app.find(NavigationMain).html()).toMatchSnapshot();
-  });
-  it('has a header', () => {
-    expect(app.find(Header).length).toBe(1);
-    expect(app.find(Header).html()).toMatchSnapshot();
-  });
-  it('has main content', () => {
-    expect(app.find(ContentMain).length).toBe(1);
-    expect(app.find(ContentMain).html()).toMatchSnapshot();
   });
 });
