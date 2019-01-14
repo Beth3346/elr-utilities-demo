5da2ee65c5dd3fc2b2d08cd5043eccdca6617a26
Elizabeth Rogers <beth3346@gmail.com>
2019-01-14 09:37:22 -0600

    added scss support added expanding line menu

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import './TabItem.scss';
 
-const TabItem = ({ current, handleClick, index, item }) => {
+const MenuItem = ({ current, handleClick, index, item }) => {
   const classes = current ? 'menu-item current' : 'menu-item';
 
   return (
     <li onClick={() => handleClick(index)} className={classes}>
-      <button className="tab-link">{item}</button>
+      <a className="menu-link" href="#">
+        {item}
+      </a>
     </li>
   );
 };
 
-export default TabItem;
+export default MenuItem;
--- src/views/AnimatedLinkExample/AnimatedLinkExample.jsx
+++ src/views/AnimatedLinkExample/AnimatedLinkExample.jsx
@@ -5,13 +5,17 @@ import ExampleSection from '@/components/ExampleSection/ExampleSection';
 import './AnimatedLinkExample.css';
 import SimpleLineTabs from '@/components/SimpleLineTabs/SimpleLineTabs';
 import DoubleLineTabs from '@/components/DoubleLineTabs/DoubleLineTabs';
+import ExpandingLineMenu from '@/components/ExpandingLineMenu/ExpandingLineMenu';
 
 const AnimatedLinkExample = () => (
   <div>
     <Header heading="Animated Links">Animated Links Example</Header>
     <ContentMain>
       <section id="animated-links">
-        <ExampleSection heading="Tab Bar Transitions" subheading="">
+        <ExampleSection
+          heading="Tab Bar Transitions"
+          subheading="Menu Transitions for Tabs"
+        >
           <div className="example-instructions">
             <p>
               These are simple line transitions that take place when a link is
@@ -36,6 +40,30 @@ const AnimatedLinkExample = () => (
           </div>
           <div className="elr-col-full example-holder">
             <DoubleLineTabs />
+          </div>
+        </ExampleSection>
+        <ExampleSection
+          heading="Navigation Menu Transitions"
+          subheading="Menu transitions for page navigation"
+        >
+          <div className="example-instructions">
+            <p>
+              These are simple line transitions that take place when a user
+              hovers over a link. These animations can be incorporated into
+              traditional navigation menus or tabbed content.
+            </p>
+            <p>
+              I recommend using some JavaScript to toggle the current class when
+              a link is clicked. This helps the user to navigation your site's
+              menu structure. Example JavaScript is included in the source code
+              below.
+            </p>
+          </div>
+          <div className="elr-col-full">
+            <h4 className="example-subheading">Expanding Line Menu</h4>
+          </div>
+          <div className="elr-col-full example-holder">
+            <ExpandingLineMenu />
           </div>
         </ExampleSection>
       </section>
