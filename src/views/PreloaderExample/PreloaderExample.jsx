a6bb3e51b493aaa21fbe4b6e16119a063b270a4d
Elizabeth Rogers <beth3346@gmail.com>
2019-02-02 23:54:15 -0600

    add svg spinner

--- src/views/PreloaderExample/PreloaderExample.jsx
+++ src/views/PreloaderExample/PreloaderExample.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from '@/components/Header/Header';
 import ContentMain from '@/components/ContentMain/ContentMain';
 import ExampleSection from '@/components/ExampleSection/ExampleSection';
+import Spinner from '@/components/Spinner/Spinner';
 
 const PreloaderExample = () => (
   <div>
     <Header heading="Preloader">Preloader Example</Header>
     <ContentMain>
       <section id="buttons">
-        <ExampleSection heading="" subheading="" />
+        <ExampleSection heading="Preloaders" subheading="">
+          <Spinner color="#1976d2" />
+        </ExampleSection>
       </section>
     </ContentMain>
   </div>
