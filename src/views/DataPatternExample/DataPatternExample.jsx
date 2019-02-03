c7923819ba7e0b6e6f2d4a72590a2d2edd2f56d5
Elizabeth Rogers <beth3346@gmail.com>
2019-02-02 20:09:40 -0600

    add styled address component

--- src/views/DataPatternExample/DataPatternExample.jsx
+++ src/views/DataPatternExample/DataPatternExample.jsx
@@ -2,16 +2,30 @@ import React from 'react';
 import Header from '@/components/Header/Header';
 import ContentMain from '@/components/ContentMain/ContentMain';
 import ExampleSection from '@/components/ExampleSection/ExampleSection';
+import AddressData from '@/components/Address/AddressData';
 
-const DataPatternExample = () => (
-  <div>
-    <Header heading="Data Patterns">Data Patterns Example</Header>
-    <ContentMain>
-      <section id="data-patterns">
-        <ExampleSection heading="Address" subheading="" />
-      </section>
-    </ContentMain>
-  </div>
-);
+const DataPatternExample = () => {
+  const address = {
+    name: 'Annie Edison',
+    street: '1000 Greendale Lane',
+    city: 'Greendale',
+    state: 'Colorado',
+    postal: '98765',
+    country: 'United States of America'
+  };
+
+  return (
+    <div>
+      <Header heading="Data Patterns">Data Patterns Example</Header>
+      <ContentMain>
+        <section id="data-patterns">
+          <ExampleSection heading="Address" subheading="">
+            <AddressData address={address} />
+          </ExampleSection>
+        </section>
+      </ContentMain>
+    </div>
+  );
+};
 
 export default DataPatternExample;
