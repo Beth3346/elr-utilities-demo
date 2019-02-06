80db8ea0348cab354aea932b12cdd96e74d6e3cd
Elizabeth Rogers <beth3346@gmail.com>
2019-02-05 23:57:56 -0600

    add elr-scss-animations for animation styles

--- src/components/Blackout/Blackout.jsx
+++ src/components/Blackout/Blackout.jsx
@@ -3,7 +3,7 @@ import './Blackout.scss';
 
 const Blackout = ({ children, show, toggle }) => {
   return (
-    <div className={`elr-blackout ${show && 'active'}`}>
+    <div onClick={() => toggle(false)} className={`elr-blackout ${show && 'active'}`}>
       <button onClick={() => toggle(false)} className="elr-blackout-close">
         &times;
       </button>
