97d1c7ac16a7ac855b8ca7a7cdc001f920f2de1f
Elizabeth Rogers <beth3346@gmail.com>
2019-02-10 15:11:08 -0600

    add elr-scss-cards add more card examples

--- src/components/ContentCard/ContentCard.jsx
+++ src/components/ContentCard/ContentCard.jsx
@@ -13,18 +13,18 @@ const ContentCard = ({
 }) => (
   <div className={classes}>
     <div className="elr-card-header">
+      {image && (
+        <figure className="elr-card-full-image">
+          <img src={image} alt="" />
+        </figure>
+      )}
+      {icon}
       <div className="elr-header-content">
         {thumbnail && (
           <figure className="elr-card-thumbnail">
             <img src={thumbnail} alt="" />
           </figure>
         )}
-        {image && (
-          <figure className="elr-card-full-image">
-            <img src={image} alt="" />
-          </figure>
-        )}
-        {icon}
         <div className="elr-card-headings">
           <h2 className="elr-card-title">{title}</h2>
           {subtitle && <h3 className="elr-card-subtitle">{subtitle}</h3>}
