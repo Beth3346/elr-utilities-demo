9b03f319d0140c4d1527c0cadab35ca5a15ee138
Elizabeth Rogers <beth3346@gmail.com>
2019-02-20 01:21:28 -0600

    add modal example

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Modal.scss';
+
+const Modal = ({ show, handleClose }) => {
+  return (
+    <div className={`elr-modal ${show && 'active'}`}>
+      <div className="elr-modal-header">
+        <button onClick={handleClose} className="elr-modal-close">
+          &times;
+        </button>
+        <div className="elr-modal-headings">
+          <h2 className="elr-modal-heading-main">Terms of Service</h2>
+          <h3 className="elr-modal-heading-sub">Please read this</h3>
+        </div>
+      </div>
+      <div className="elr-modal-body">
+        <div className="elr-modal-content">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum
+            tenetur cumque veritatis reprehenderit odit nobis natus dolores
+            ullam repellendus, unde vero maiores, molestiae fuga quaerat vel,
+            provident nostrum assumenda voluptates.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            quod quasi molestias id beatae deserunt consectetur possimus ex
+            perferendis fugiat!
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum
+            tenetur cumque veritatis reprehenderit odit nobis natus dolores
+            ullam repellendus, unde vero maiores, molestiae fuga quaerat vel,
+            provident nostrum assumenda voluptates.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            quod quasi molestias id beatae deserunt consectetur possimus ex
+            perferendis fugiat!
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum
+            tenetur cumque veritatis reprehenderit odit nobis natus dolores
+            ullam repellendus, unde vero maiores, molestiae fuga quaerat vel,
+            provident nostrum assumenda voluptates.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            quod quasi molestias id beatae deserunt consectetur possimus ex
+            perferendis fugiat!
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum
+            tenetur cumque veritatis reprehenderit odit nobis natus dolores
+            ullam repellendus, unde vero maiores, molestiae fuga quaerat vel,
+            provident nostrum assumenda voluptates.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            quod quasi molestias id beatae deserunt consectetur possimus ex
+            perferendis fugiat!
+          </p>
+        </div>
+      </div>
+      <div className="elr-modal-footer">
+        <button className="elr-button elr-button-primary">Accept</button>
+        <button className="elr-button elr-button-danger">Cancel</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/views/ModalExample/ModalExample.jsx
+++ src/views/ModalExample/ModalExample.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '@/components/Header/Header';
 import ContentMain from '@/components/ContentMain/ContentMain';
 import ExampleSection from '@/components/ExampleSection/ExampleSection';
+import Modal from '@/components/Modal/Modal';
 
-const ModalExample = () => (
-  <div>
-    <Header heading="Modal">Modal Example</Header>
-    <ContentMain>
-      <section id="accordion">
-        <ExampleSection heading="Modal" subheading="" />
-      </section>
-    </ContentMain>
-  </div>
-);
+const ModalExample = () => {
+  const [show, setShow] = useState(true);
 
+  return (
+    <div>
+      <Header heading="Modal">Modal Example</Header>
+      <ContentMain>
+        <section id="accordion">
+          <ExampleSection heading="Modal" subheading="">
+            <button
+              onClick={() => setShow(true)}
+              className="elr-button elr-button-success"
+            >
+              Show Modal
+            </button>
+            <Modal handleClose={() => setShow(false)} show={show} />
+          </ExampleSection>
+        </section>
+      </ContentMain>
+    </div>
+  );
+};
 export default ModalExample;
