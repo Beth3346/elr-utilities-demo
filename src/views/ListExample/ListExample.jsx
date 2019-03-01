55bb8014e7d652be3ce24a896dea3c0d075cfa06
Elizabeth Rogers <beth3346@gmail.com>
2019-03-01 17:33:59 -0600

    add lists

--- src/views/ListExample/ListExample.jsx
+++ src/views/ListExample/ListExample.jsx
@@ -7,8 +7,15 @@ const ListExample = () => (
   <div>
     <Header heading="Lists">Lists Example</Header>
     <ContentMain>
-      <section id="buttons">
-        <ExampleSection heading="" subheading="" />
+      <section id="lists">
+        <ExampleSection heading="Lists" subheading="some lists">
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+          </ul>
+        </ExampleSection>
       </section>
     </ContentMain>
   </div>
